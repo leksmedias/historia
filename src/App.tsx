54cba9b634958543824c5a3829114f8e1391a452
leksi <woofiverr@gmail.com>
2026-06-02 10:23:17 +0100

    feat: wire AuthProvider, ProtectedRoute, setup/login routes into App

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "@/components/AppLayout";
 import { GenerationProvider } from "@/lib/GenerationContext";
+import { AuthProvider } from "@/lib/AuthContext";
+import { ProtectedRoute } from "@/components/ProtectedRoute";
 import Index from "./pages/Index";
 import Projects from "./pages/Projects";
 import ProjectStatus from "./pages/ProjectStatus";
@@ -14,34 +16,49 @@ import ErrorLog from "./pages/ErrorLog";
 import JsonToVideo from "./pages/JsonToVideo";
 import ImageModelTest from "./pages/ImageModelTest";
 import ScriptToJson from "./pages/ScriptToJson";
+import Setup from "./pages/Setup";
+import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <GenerationProvider>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <AppLayout>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/projects/:projectId" element={<ProjectStatus />} />
-            <Route path="/projects/:projectId/preview" element={<ProjectPreview />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/errors" element={<ErrorLog />} />
-            <Route path="/json-to-video" element={<JsonToVideo />} />
-            <Route path="/image-test" element={<ImageModelTest />} />
-            <Route path="/script-to-json" element={<ScriptToJson />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AppLayout>
-      </BrowserRouter>
-    </TooltipProvider>
-    </GenerationProvider>
+    <AuthProvider>
+      <GenerationProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/setup" element={<Setup />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/*"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout>
+                      <Routes>
+                        <Route path="/" element={<Index />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/projects/:projectId" element={<ProjectStatus />} />
+                        <Route path="/projects/:projectId/preview" element={<ProjectPreview />} />
+                        <Route path="/settings" element={<Settings />} />
+                        <Route path="/errors" element={<ErrorLog />} />
+                        <Route path="/json-to-video" element={<JsonToVideo />} />
+                        <Route path="/image-test" element={<ImageModelTest />} />
+                        <Route path="/script-to-json" element={<ScriptToJson />} />
+                        <Route path="*" element={<NotFound />} />
+                      </Routes>
+                    </AppLayout>
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </GenerationProvider>
+    </AuthProvider>
   </QueryClientProvider>
 );
 
